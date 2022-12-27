ee91ff467da439c9b12698a66bb39405d954c4e5
Abhishek Gautam <83368305+Abhigautam6517@users.noreply.github.com>
2022-12-27 22:27:18 +0530

    Add files via upload

---
+++ Array/MaximunIndex.java
@@ -0,0 +1,29 @@
+class Solution{
+    static int maxIndexDiff(int A[], int N) { 
+        int k = 0;
+        for(int i = N-1; i>0; i--)
+        {
+            if(A[i]>=A[0])
+            {
+                k = i;
+                break;
+            }
+
+        }
+        for(int i = 1; i<N-k-1; i++)
+        {
+            for(int j = N-1; j>k;j--)
+            {
+                if(A[i]<A[j])
+                {
+                    k = Math.max(j-i,k);
+                    break;
+                }
+            }
+        }
+        return k;
+    
+        
+    }
+}
+
